8660f24de4a14e20486b69213181283f0586872c
Daniel Schröder <32933505+daniel-schroeder-zeiss@users.noreply.github.com>
2024-06-18 17:25:21 +0200

    feat: Add method for data parsing to IImportFormat interface

--- src/PiWeb.Import.Sdk/IPlugin.cs
+++ src/PiWeb.Import.Sdk/IPlugin.cs
@@ -20,8 +20,8 @@ public interface IPlugin
 	#region methods
 
 	/// <summary>
-	/// Initializes the plugin. Usually called during startup of the hosting application while showing a splash screen. Startup finishes
-	/// when the returned task is completed.
+	/// Initializes the plugin. Usually called during startup of the hosting application while showing a splash screen.
+	/// Startup finishes when the returned task is completed.
 	/// </summary>
 	/// <param name="context">Contains information about the hosting application.</param>
 	/// <exception cref="ModuleRegistrationException">Thrown when module registration fails.</exception>
---
+++ src/PiWeb.Import.Sdk/Modules/ImportFormat/IParseContext.cs
@@ -0,0 +1,35 @@
+﻿#region copyright
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Carl Zeiss Industrielle Messtechnik GmbH        */
+/* Softwaresystem PiWeb                            */
+/* (c) Carl Zeiss 2024                             */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#endregion
+
+using Zeiss.PiWeb.Import.Sdk.Environment;
+using Zeiss.PiWeb.Import.Sdk.Logging;
+
+namespace Zeiss.PiWeb.Import.Sdk.Modules.ImportFormat;
+
+/// <summary>
+/// Represents the context of an import runner provided by the hosting application.
+/// </summary>
+public interface IParseContext
+{
+	#region properties
+
+	/// <summary>
+	/// Contains information about the environment a plugin is hosted in.
+	/// </summary>
+	IEnvironmentInfo EnvironmentInfo { get; }
+
+	/// <summary>
+	/// A logger that can be used to write log entries. Written entries are usually forwarded to the log file
+	/// of the hosting application.
+	/// </summary>
+	ILogger Logger { get; }
+
+	#endregion
+}
