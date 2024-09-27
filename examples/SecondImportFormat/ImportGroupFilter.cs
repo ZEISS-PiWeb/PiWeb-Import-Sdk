1dfcfadb149bdae968b991d9a5b84342a24972d8
David Dombrowe <david-dombrowe>
2024-09-27 14:27:27 +0200

    Add example plug-in for import format plug-ins used in fundamentals section of the documentation

---
+++ examples/SecondImportFormat/ImportGroupFilter.cs
@@ -0,0 +1,38 @@
+﻿#region copyright
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Carl Zeiss Industrielle Messtechnik GmbH        */
+/* Softwaresystem PiWeb                            */
+/* (c) Carl Zeiss 2024                             */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#endregion
+
+#region usings
+
+using System.IO;
+using System.Threading.Tasks;
+using Zeiss.PiWeb.Sdk.Import.ImportFiles;
+
+#endregion
+
+namespace Zeiss.SecondImportFormat
+{
+    public class ImportGroupFilter : IImportGroupFilter
+    {
+        public async ValueTask<FilterResult> FilterAsync(IImportGroup importGroup, IFilterContext context)
+        {
+            // Check file extension.
+            if (!importGroup.PrimaryFile.HasExtension(".txt"))
+                return FilterResult.None;
+
+            // Add additional data to import group.
+            var additionalData = context.CurrentImportFolder.FindFile(importGroup.PrimaryFile.BaseName + ".png");
+            if (additionalData == null)
+                return FilterResult.RetryOrImport;
+            importGroup.AddFile(additionalData);
+
+            return FilterResult.Import;
+        }
+    }
+}
---
+++ examples/SecondImportFormat/ImportParser.cs
@@ -0,0 +1,85 @@
+﻿#region copyright
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Carl Zeiss Industrielle Messtechnik GmbH        */
+/* Softwaresystem PiWeb                            */
+/* (c) Carl Zeiss 2024                             */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#endregion
+
+#region usings
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Zeiss.PiWeb.Sdk.Import.ImportData;
+using Zeiss.PiWeb.Sdk.Import.ImportFiles;
+using Zeiss.PiWeb.Sdk.Import.Modules.ImportFormat;
+
+#endregion
+
+namespace Zeiss.SecondImportFormat
+{
+    public class ImportParser : IImportParser
+    {
+        public async Task<ImportData> ParseAsync(IImportGroup importGroup, IParseContext context,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            // Create root part and measurement.
+            var root = new InspectionPlanPart(importGroup.PrimaryFile.BaseName);
+            var measurement = root.AddMeasurement();
+
+            // Create reader for import file.
+            await using var stream = importGroup.PrimaryFile.GetDataStream();
+            using var reader = new StreamReader(stream);
+
+            string? line;
+
+            // Parse header attributes.
+            while ((line = reader.ReadLine()) != null)
+            {
+                if( string.IsNullOrEmpty(line))
+                    continue;
+
+                if (line.StartsWith("#Characteristic"))
+                    break;
+
+                var rowItems = line.Split(": ");
+                var attribute = rowItems[0].Trim();
+                var value = rowItems[1].Trim();
+
+                switch (attribute)
+                {
+                    case "Date":
+                        measurement.SetVariable("Date",value);
+                        break;
+                    case "Text":
+                        measurement.SetVariable("Text",value);
+                        break;
+                }
+            }
+
+            // Parse measured value for each characteristic.
+            while ((line = reader.ReadLine()) != null)
+            {
+                if( string.IsNullOrEmpty(line))
+                    continue;
+
+                var rowItems = line.Split(',');
+                var characteristicName = rowItems[0].Trim();
+                var value = rowItems[1].Trim();
+
+                var characteristic = root.AddCharacteristic(characteristicName);
+                var measuredValue = measurement.AddMeasuredValue(characteristic);
+                measuredValue.SetVariable("Value",double.Parse(value));
+            }
+
+            // Add additional data.
+            foreach (var rawData in importGroup.AdditionalFiles)
+                measurement.AddAdditionalData(rawData.Name, rawData.GetDataStream());
+
+            return new ImportData(root);
+        }
+    }
+}
