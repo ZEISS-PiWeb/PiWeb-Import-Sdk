f3539297d617faf5f1f76ec4a8a2c9724015870b
Daniel Schröder <32933505+daniel-schroeder-zeiss@users.noreply.github.com>
2026-01-13 15:18:18 +0100

    fix: Fix issues in example plugins

--- examples/SimpleTxtPlugin/ImportFormat.cs
+++ examples/SimpleTxtPlugin/ImportFormat.cs
@@ -8,6 +8,7 @@
 
 #endregion
 
+using System.Collections.Immutable;
 using Zeiss.PiWeb.Sdk.Import.ImportFiles;
 using Zeiss.PiWeb.Sdk.Import.Modules.ImportFormat;
 
@@ -23,5 +24,24 @@ public sealed class ImportFormat : IImportFormat
     public IImportParser CreateImportParser(ICreateImportParserContext context)
     {
         return new ImportParser();
+    }
+
+    public IImportFormatConfiguration CreateConfiguration(ICreateImportFormatConfigurationContext context)
+    {
+        return new ImportFormatConfiguration
+            {
+                SupportsAttributeMapping = true,
+                DefaultAttributeMappingConfiguration = new AttributeMappingConfiguration
+                {
+                    AutoMapping = true,
+                    MappingRules = new[] {
+                        new MappingRule { AttributeKey = 1, MappingTarget = MappingTarget.Measurement, ValueExpression = "${ Value }" },
+                        new MappingRule { AttributeKey = 2101, MappingTarget = MappingTarget.Characteristic, ValueExpression = "${ Nominal }" },
+                        new MappingRule { AttributeKey = 2543, MappingTarget = MappingTarget.Characteristic, ValueExpression = "${ XPosition }" },
+                        new MappingRule { AttributeKey = 2544, MappingTarget = MappingTarget.Characteristic, ValueExpression = "${ YPosition }" },
+                        new MappingRule { AttributeKey = 2545, MappingTarget = MappingTarget.Characteristic, ValueExpression = "${ ZPosition }" }
+                    }.ToImmutableList()
+                }
+            };
     }
 }
